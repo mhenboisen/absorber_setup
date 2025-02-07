c119cb367172ba254f8aa54c87297d5562d00b43
mhenboisen <q15mhenboisen@wgsf.net>
2025-02-07 12:56:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from pygame_functions import *
 import math, random, time, pickle
 
-screenSize(1000,1000)
+sensitivity = 15
+max_speed = 15
+screen_width = 1920
+screen_height = 1080
+
+
+screenSize(screen_width,screen_height)
 setBackgroundColour("black")
 creatures = []
 
@@ -10,29 +16,24 @@ class Creature:
         self.x = x
         self.y = y
         self.velocity = velocity # as an ij vector
-        self.size =size   # size is a percentage of the full size image
+        self.size = size   # size is a percentage of the full size image
         self.the_power_up = None 
         self.sprite = makeSprite(image)
         moveSprite(self.sprite,self.x,self.y,centre=True)
-        transformSprite(self.sprite, 0, self.size/100)
+        transformSprite(self.sprite, 0, self.size/25)
         showSprite(self.sprite)
         
-    def move(self):
-        if self.x + self.velocity[0] > 1100:
-            self.x = -100
-        elif self.x + self.velocity[0] < -100:
-            self.x = 1100
+    def move(self, player):
+        self.x = (self.x + self.velocity[0]) % 10000
+        self.y = (self.velocity[1]+ self.y) % 10000
         
-        if self.y + self.velocity[1] > 1100:
-            self.y = -100
-        elif self.y + self.velocity[1] < -100:
-            self.y = 1100
+        self.x_onscreen = self.x - player.x + screen_width/2
+        self.y_onscreen = self.y - player.y  + screen_height/2
         
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
-        moveSprite(self.sprite,self.x,self.y,centre=True)
-    def update(self):
-        self.move()
+
+        moveSprite(self.sprite,self.x_onscreen,self.y_onscreen,centre=True)
+    def update(self,player):
+        self.move(player)
         
 class Enemy(Creature):
     def __init__(self,x,y,image,velocity,size):
@@ -42,53 +43,77 @@ class Enemy(Creature):
 class Player(Creature):
     def __init__(self,x,y,image,velocity,size):
         super().__init__(x,y,image,velocity,size)
+        self.angle = 0
+        moveSprite(self.sprite,screen_width/2,screen_height/2,centre=True)
+        transformSprite(self.sprite, 0, self.size/100)
         
     def move(self):
         #calculates the x and y displacement between player and mouse
-        x_diff = (mouseX() - self.x)/self.size
-        y_diff = (mouseY() -self.y)/self.size
+        x_diff = (mouseX() - screen_width/2)/sensitivity
+        y_diff = (mouseY() - screen_height/2)/sensitivity
         
         #limits the speed to 10px per tick
         if x_diff >= 0:
-            self.velocity[0] = min(50,x_diff)
+            self.velocity[0] = min(max_speed,x_diff)
         else:
-            self.velocity[0] = max(-50,x_diff)
+            self.velocity[0] = max(-max_speed,x_diff)
         
         if y_diff >= 0:
-            self.velocity[1] = min(50,y_diff)
+            self.velocity[1] = min(max_speed,y_diff)
         else:
-            self.velocity[1] = max(-50,y_diff)
-
-        #updates the position of the player
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
-        moveSprite(self.sprite,self.x,self.y,centre=True)
+            self.velocity[1] = max(-max_speed,y_diff)
         
-    def collision(self,object):
-        if self.size > object.size:
-            self.size += 1
-            object.die()
-        elif self.size < object.size:
-            self.die()
-    def update(self):
+        self.x = (self.x + self.velocity[0]) % 10000
+        self.y = (self.velocity[1]+ self.y) % 10000
+        
+        self.angle = math.degrees(math.atan2(y_diff, x_diff))
+        transformSprite(self.sprite, self.angle, self.size/100)        
+    
+    def collision(self,creatures):
+        for c in creatures:
+            if touching(self.sprite,c.sprite):
+                print(str(self.size),str(c.size))
+                if self.size > c.size:
+                    print("Nom")
+                    creatures.remove(c)
+                    hideSprite(c.sprite)
+                    self.size = self.size + c.size/10
+                    transformSprite(self.sprite, self.angle, self.size/100)
+                elif self.size < c.size:
+                    print("dead")
+                    self.die()
+    def update(self,creatures):
         self.move()
+        self.collision(creatures)
         
+    def die(self):
+        global gameOver
+        gameOver = False
+
+def drawBoundary(player):
+    clearShapes()
+    drawRect(screen_width/2-player.x,screen_height/2-player.y,10000,10000, (0,0,40), 0)
+    drawRect(screen_width/2-player.x,screen_height/2-player.y,10000,10000, (255,255,255), 5)
 
 setAutoUpdate(False)
-for i in range(10):
-    creatures.append(Enemy(random.randint(0,1000),random.randint(0,1000), "enemy.png", [random.choice([-1,1])*random.randint(2,4),random.choice([-1,1])*random.randint(2,4)],random.randint(50,100)) )
+for i in range(300):
+    creatures.append(Enemy(random.randint(0,10000),random.randint(0,10000), "enemy.png", [random.choice([-1,1])*random.randint(2,4),random.choice([-1,1])*random.randint(2,4)],random.randint(5,100)) )
 
-p1 = Player(100,100,"player.png",[0,0],10)
+p1 = Player(5000,5000,"player.png",[0,0],10)
 gameOver = False
-
+initial_time = 0
+end_time = 1
 while not gameOver:
+    initial_time = clock()
+    fps = 1/(end_time - initial_time)
     tick(30)
+    drawBoundary(p1)
     for creature in creatures:
-        creature.update()
-    p1.update()
+        creature.update(p1)
+    p1.update(creatures)
     
     updateDisplay()
-
+    
         
 endWait()
 
